40dece56ab2763aa6aa71a170bf5f3fd329e3e9d
Leonardo Sanches <sleonardogabriel@gmail.com>
2023-01-11 16:26:47 -0300

    SEO para algumas páginas

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '../styles/global'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../lib/react-query'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles()
 
@@ -15,6 +16,14 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://ignite-call.leonardo.com.br',
+            siteName: 'Ignite call',
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
